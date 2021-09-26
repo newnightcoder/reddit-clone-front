a585218294c13c67ea644b15942a00a929385eec
Nightcoder <newnightcoder@gmail.com>
2021-09-26 11:24:38 +0200

    post styling! :art:

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,58 @@
 import React from "react";
+import {
+  ChatRight,
+  HandThumbsUp,
+  ThreeDotsVertical,
+} from "react-bootstrap-icons";
 import picPlaceholder from "../assets/pic_placeholder.svg";
 
 const picUrl = "";
 
 const Post = ({ post }) => {
   return (
-    <div className="h-max w-11/12 flex flex-col items-center justify-center bg-white border border-gray-300 rounded">
-      <div
-        className="w-10 h-10 rounded-full border border-gray-300"
-        style={
-          picUrl !== ""
-            ? { background: `url(${picUrl}) no-repeat center/cover` }
-            : {
-                background: `url(${picPlaceholder}) no-repeat center/cover`,
-              }
-        }
-      ></div>
-      <div>{post.username}</div>
-      <div>{post.title}</div>
-      <div>{post.text}</div>
-      <div>{post.date}</div>
+    <div className="post-container h-max w-11/12 flex-col items-center justify-center bg-white border border-gray-300 rounded-md px-2 pt-2">
+      <div className="top w-full flex items-center justify-center pb-1 border-b">
+        <div className="left-column h-full w-2/12 flex justify-center">
+          <div
+            className="avatar-container w-11 h-11 rounded-full border border-gray-300"
+            style={
+              picUrl !== ""
+                ? { background: `url(${picUrl}) no-repeat center/cover` }
+                : {
+                    background: `url(${picPlaceholder}) no-repeat center/cover`,
+                  }
+            }
+          ></div>
+        </div>
+        <div className="right-column  h-full w-10/12 flex flex-col items-center justify-center">
+          <div className="username-title-container h-12 w-full flex flex-col items-start justify-center pl-1 pr-3">
+            <div className="username-date w-full flex items-center justify-between gap-2">
+              <div className="capitalize">
+                <span className="text-xs">@</span>
+                {post.username}
+              </div>
+              <div className="text-xs italic">{post.date}</div>
+            </div>
+            <div className="title font-bold">{post.title}</div>
+          </div>
+        </div>
+      </div>
+      <div className="text w-full text-left px-4 py-2 text-sm">{post.text}</div>
+      <div className="bottom w-full flex items-center justify-end px-2 py-2 border-t">
+        <div className="icons-container w-max flex items-center justify-end gap-4 text-xs">
+          <div className="w-max flex items-center justify-center gap-1">
+            <ChatRight />
+            <span>Commenter</span>
+          </div>
+          <div className="w-max flex items-center justify-center gap-1">
+            <HandThumbsUp />
+            <span>Liker</span>
+          </div>
+          <div className="w-max flex items-center justify-center gap-1">
+            <ThreeDotsVertical />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
