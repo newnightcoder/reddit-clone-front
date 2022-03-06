612b7096f433315c6f9f3b55cbfe989c5ff0a12d
Nightcoder <newnightcoder@gmail.com>
2022-03-07 00:42:49 +0100

    netlify.toml file added

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -1,2 +1,4 @@
 export const API_AUTH = "https://social-media-sql-backend.herokuapp.com/auth";
 export const API_POST = "https://social-media-sql-backend.herokuapp.com/post";
+// export const API_AUTH = "http://localhost:3001/auth";
+// export const API_POST = "http://localhost:3001/post";
