df891a573b6976a3907359c05f148ef77be872bf
Nightcoder <newnightcoder@gmail.com>
2022-03-06 11:16:30 +0100

    api endpoint url modified for heroku backend :white_check_mark:

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -1,2 +1,2 @@
-export const API_AUTH = "http://localhost:3001/auth";
-export const API_POST = "http://localhost:3001/post";
+export const API_AUTH = "https://social-media-sql-backend.herokuapp.com/auth";
+export const API_POST = "https://social-media-sql-backend.herokuapp.com/post";
