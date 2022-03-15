eb9655bbfba0de62ee55dbf9ec9cef5640136c37
Nightcoder <newnightcoder@gmail.com>
2022-03-15 19:48:09 +0100

    homepage design ok :art:

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as apple } from "./Apple-store-icon.png";
+export { default as logo } from "./forum-logo.svg";
+export { default as google } from "./Google-Play-Icon.png";
+export { default as phonesImg } from "./phones.png";
--- src/components/NavBarDesktop.jsx
+++ src/components/NavBarDesktop.jsx
@@ -10,7 +10,7 @@ const NavBarDesktop = () => {
   const linkToProfile = useLinkToProfile();
 
   return (
-    <div className="w-max hidden md:flex flex-col items-center justify-center space-y-4 sticky top-20 border border-gray-30 px-4 lg:pt-6 pb-4 whitespace-nowrap rounded-lg lg:bg-white">
+    <div className="w-max hidden md:flex flex-col items-center justify-center space-y-4 sticky top-20 border border-gray-30 px-4 lg:pt-6 pb-4 whitespace-nowrap rounded-lg lg:bg-white border border-gray-400">
       <Link to="/create" className="flex items-center justify-center lg:justify-start lg:pr-4 rounded-full bg-gray-400">
         <div className="w-16 h-16 lg:w-10 lg:h-10 rounded-full relative flex items-center justify-center bg-gray-400">
           <div className="inline-block absolute top-0 left-0 transform translate-x-4 translate-y-2 lg:translate-x-2 lg:translate-y-0 text-white lg:text-gray-900">
