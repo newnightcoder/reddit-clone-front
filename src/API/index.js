798248cd995e62f874ecc1e7fa3e50072e3f2f1d
Nightcoder <newnightcoder@gmail.com>
2022-04-06 18:48:47 +0200

    api endpoints modified to heroku

--- src/API/index.js
+++ src/API/index.js
@@ -1,11 +1,5 @@
-export const API_AUTH =
-  process.env.NODE_ENV === "production"
-    ? "https://social-media-sql-backend.herokuapp.com/api/auth"
-    : process.env.NODE_ENV === "development" && "http://localhost:3001/api/auth";
-export const API_POST =
-  process.env.NODE_ENV === "production"
-    ? "https://social-media-sql-backend.herokuapp.com/api/post"
-    : process.env.NODE_ENV === "development" && "http://localhost:3001/api/post";
+export const API_AUTH = "https://social-media-sql-backend.herokuapp.com/api/auth";
+export const API_POST = "https://social-media-sql-backend.herokuapp.com/api/post";
 // export const API_AUTH = "https://social-media-sql-backend.herokuapp.com/api/auth";
 // export const API_POST = "https://social-media-sql-backend.herokuapp.com/api/post";
 // export const API_AUTH = "http://localhost:3001/api/auth";
