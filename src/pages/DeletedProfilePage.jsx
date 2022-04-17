3ce49e05c9e509f06b5d1320103e2a651a686a7c
Nightcoder <newnightcoder@gmail.com>
2022-04-17 22:56:09 +0200

    bg img deleted

--- src/pages/DeletedProfilePage.jsx
+++ src/pages/DeletedProfilePage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import Div100vh from "react-div-100vh";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useLocation } from "react-router";
-import bg from "../assets/bg.webp";
+import { useLocation } from "react-router";
 import { getPosts } from "../store/actions/posts.action";
 import { history } from "../utils/helpers";
 import { useLanguage } from "../utils/hooks";
@@ -22,29 +21,26 @@ const DeletedProfile = () => {
 
   return (
     <>
-      {!isAuthenticated ? (
+      {/* {!isAuthenticated ? (
         <Redirect to={{ pathname: "/" }} />
-      ) : (
-        <Div100vh
-          className="page-container min-h-screen w-screen flex flex-col items-center justify-center relative pb-8 overflow-x-hidden"
-          style={{ background: `linear-gradient(rgba(70,70,70,.45), rgba(70,70,70,.45)), url(${bg}) no-repeat center/cover` }}
-        >
-          <div className="h-1/4 w-10/12 flex flex-col items-center justify-center rounded-sm bg-red-400 text-black py-2 px-2">
-            {!isAdmin ? (
-              <>
-                {userLanguage.deletePage.confirmation}
-                <br /> {userLanguage.deletePage.missYou} <br />
-                {userLanguage.deletePage.hope}
-              </>
-            ) : (
-              <>{userLanguage.deletePage.modMsg}</>
-            )}
-          </div>
-          <button className="hover:cursor-pointer hover:underline" onClick={() => history.push(!isAdmin ? "/" : "/feed")}>
-            {userLanguage.deletePage.backBtn}
-          </button>
-        </Div100vh>
-      )}
+      ) : ( */}
+      <Div100vh className="page-container min-h-screen w-screen flex flex-col items-center justify-center relative pb-8 overflow-x-hidden">
+        <div className="h-1/4 w-10/12 flex flex-col items-center justify-center rounded-sm bg-red-400 text-black py-2 px-2">
+          {!isAdmin ? (
+            <>
+              {userLanguage.deletePage.confirmation}
+              <br /> {userLanguage.deletePage.missYou} <br />
+              {userLanguage.deletePage.hope}
+            </>
+          ) : (
+            <>{userLanguage.deletePage.modMsg}</>
+          )}
+        </div>
+        <button className="hover:cursor-pointer hover:underline" onClick={() => history.push(!isAdmin ? "/" : "/feed")}>
+          {userLanguage.deletePage.backBtn}
+        </button>
+      </Div100vh>
+      {/* )} */}
     </>
   );
 };
