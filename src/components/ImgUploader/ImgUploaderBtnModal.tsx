dba25ac98d9ca10ece09e105ff9cc4fdc1d204f3
Nightcoder <newnightcoder@gmail.com>
2022-09-11 04:21:35 +0200

    imgUploaderBtns corrections/meta theme color

--- src/components/ImgUploader/ImgUploaderBtnModal.tsx
+++ src/components/ImgUploader/ImgUploaderBtnModal.tsx
@@ -81,8 +81,8 @@ const ImgUploaderBtnModal = ({
         <XIcon className="h-5 text-black dark:text-white" />
       </button>
       <span
-        className={`w-full truncate ${
-          profilePage ? "h-8 text-xs pl-2" : ""
+        className={`h-8 w-full truncate ${
+          profilePage ? " text-xs pl-2" : ""
         } text-xs text-black dark:text-white italic text-center pr-6`}
       >
         {blobName}
